500dcb5b63cb7a94365862cdf459fa3bb7abec78
root <root@d75971cf0f8f>
2020-05-13 02:57:30 +0000

    quiz game

--- src/section3/MadlibsOfTheAmazon.java
+++ src/section3/MadlibsOfTheAmazon.java
@@ -9,25 +9,26 @@ public class MadlibsOfTheAmazon {
 		// If you find yourself having to cross a piranha-infested river, here's how to do it...
 		JOptionPane.showMessageDialog(null, "If you find yourself having to cross a pirhana-infested river, here's how to do it...");
 		// Get the user to enter an adjective
-		
+		String adj=JOptionPane.showInputDialog("Enter an Adjective");
 		// Get the user to enter a type of liquid
-
+		String liquid=JOptionPane.showInputDialog("Name a type of liquid");
 		// Get the user to enter a body part
-
+		String bp=JOptionPane.showInputDialog("Name a body part");
 		// Get the user to enter a verb
-
+		String vb=JOptionPane.showInputDialog("Enter a verb");
 		// Get the user to enter a place
-
+		String place=JOptionPane.showInputDialog("Name a place");
 		// Fit the user's words into this sentence, and save it in a String:
 		// Piranhas are more [adjective] during the day, so cross the river at
-		// night. Piranhas are attracted to fresh [type of liquid] and will most
+		// night. Piranhas are attracted to fresh [liquid] and will most
 		// likely take a bite out of your [body part] if you [verb]. Whatever
 		// you do, if you have an open wound, try to find another way to get
 		// back to the [place]. Good luck!
 		
 		// Make a pop-up for the final story. You can use \n to go to the next line
-		
+		JOptionPane.showMessageDialog(null, "Pirhanas are more " + adj + " during the day, so cross the river at night. \n Piranhas are attracted to fresh " + liquid + " and will most likely take a bite out of your " + bp + " if you " + vb + ". \n Whatever you do, if you have an open wound, try to find another way to get back to the " + place + ". Good luck!" );
 
 	}
 }
 
+	// yeah it works
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,20 +1,34 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+		int score= 0;
 		// 2.  Ask the user a question 
-		
+		String color = JOptionPane.showInputDialog("What is Sam's favorite color?");
 		// 3.  Use an if statement to check if their answer is correct
-		
+		if(color.equals("green")) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+			score += 1;
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "The correct answer was green!");
+			score -= 1;
+		}
 		// 4.  if the user's answer was correct, add one to their score 
 		
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
-		
+		String subject= JOptionPane.showInputDialog("What is Samantha's favorite subject?");
+		if(color.contentEquals("science")) {
+			JOptionPane.
+		}
 		// 6.  After all the questions have been asked, print the user's score 
+		
+		
 		
 	}
 }
